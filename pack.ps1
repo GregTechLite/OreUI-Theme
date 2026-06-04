6c176103b26ae43aad566c4d0e2acd256aa397e1
Magic_Sweepy <Magic_Sweepy@proton.me>
2026-06-04 12:20:33 +0800

    fix: Make zip format compatible with mc parser

--- pack.ps1
+++ pack.ps1
@@ -17,7 +17,25 @@ Copy-Item -Recurse "assets" "${TempDir}/"
 Copy-Item "pack.png" "${TempDir}/"
 Copy-Item "pack.mcmeta" "${TempDir}/"
 
-Compress-Archive -Path "${TempDir}/*" -DestinationPath $Output -Force
+if (Test-Path $Output) { Remove-Item $Output }
+
+[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression') | Out-Null
+[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
+
+$archive = [System.IO.Compression.ZipFile]::Open($Output, 1)
+$tempBase = (Resolve-Path $TempDir).Path
+
+Get-ChildItem -Recurse -File $TempDir | ForEach-Object {
+    $relativePath = ($_.FullName.Substring($tempBase.Length + 1) -replace '\\', '/')
+    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($archive, $_.FullName, $relativePath) | Out-Null
+}
+
+Get-ChildItem -Recurse -Directory $TempDir | ForEach-Object {
+    $relativePath = ($_.FullName.Substring($tempBase.Length + 1) -replace '\\', '/') + '/'
+    $archive.CreateEntry($relativePath) | Out-Null
+}
+
+$archive.Dispose()
 
 Remove-Item -Recurse -Force $TempDir
 
